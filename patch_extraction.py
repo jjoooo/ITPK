d817f9c9e926e2e13176ba59bdbc88e07cc9d18d
jui <kjhachhh@gmail.com>
2018-01-15 10:31:51 +0900

    Add files via upload

--- patch_extraction.py
+++ patch_extraction.py
@@ -42,12 +42,14 @@ class Patches3d(object):
         c_l = []
         for c in range(self.num_class):
             c_l.append(np.argwhere(volume_l == c))
+            print('class {} - {}'.format(c,len(c_l[c])))
             if self.num_patch > len(c_l[c]):
                 self.num_patch = len(c_l[c])
 
+        print('num patch = {}'.format(self.num_patch))
         patches, labels, center_l = [], [], []
         for c in range(self.num_class):
-        
+            print('CLASS={}'.format(c))
             # random patch each class
             cnt = 0
 
@@ -60,27 +62,25 @@ class Patches3d(object):
                 h2 = l_idx[1]+int(self.h/2)
                 w1 = l_idx[2]-int(self.w/2)
                 w2 = l_idx[2]+int(self.w/2)
-
                 if d1 < 0 or d2 > self.volume_size[0] or h1 < 0 or h2 > self.volume_size[1] or w1 < 0 or w2 > self.volume_size[2]:
                     continue
-
+  
                 label = volume_l[d1:d2, h1:h2, w1:w2]
-                cl = volume_l[l_idx[0],l_idx[1],l_idx[2]]
 
                 # label filtering
-                if not self._labels_filtering(label, cl):
+                if not self._labels_filtering(label, c):
                     continue
-
+      
                 # patch filtering
                 bool_p = True
                 for m in range(self.num_mode-1):
                     patch = volume[m, d1:d2, h1:h2, w1:w2]
-                    if not self._patch_filtering(patch):
+                    if not self._patch_filtering(patch,c):
                         bool_p = False
                         break
                 if not bool_p:
                     continue
-                
+           
                 for m in range(self.num_mode-1):
                     patch = volume[m, d1:d2, h1:h2, w1:w2]
                     if m==0:
@@ -89,24 +89,24 @@ class Patches3d(object):
                         patch_mode = np.concatenate((patch_mode, patch))
                 patches.append(patch_mode)
                 labels.append(label)
-                center_l += [cl]
+                center_l += [c]
                 cnt += 1
                     
         return patches, labels, center_l
 
-    def _labels_filtering(self, label, cl):
+    def _labels_filtering(self, label, c):
 
         # label filtering
-        if len(np.unique(label)) == 1:
+        if len(np.unique(label)) == 1 and c==0:
             a = random.randint(0,10)
             if a < 8: return False
 
-        if len(np.argwhere(label == cl)) < 10:
+        if len(np.argwhere(label == c)) < 10:
             return False
-        
+        '''
         # 80th entropy percentile
         m_ent = 0
-        ent = np.ndarray([self.h,self.w])
+        ent = np.zeros([self.h,self.w])
         for i in range(self.d):
             l_ent = entropy(label[i].astype(int), disk(self.h))
             if m_ent < np.mean(l_ent):
@@ -118,13 +118,14 @@ class Patches3d(object):
         # if 80th entropy percentile = 0
         if top_ent == 0:
             return False   
-
+        '''
         return True
 
-    def _patch_filtering(self, patch):
+    def _patch_filtering(self, patch, c):
 
         # any patch is too small 
         if patch.shape != self.patch_size:
+            print('patch shape mismatch')
             return False
 
         # 80th percentile
@@ -132,23 +133,24 @@ class Patches3d(object):
 
         # if 80th percentile = 0
         if t == -1:
+            print('percentile 90 == -1')
             return False
-
+        
         # 80th entropy percentile
-        m_ent = 0
-        ent = np.ndarray([self.h,self.w])
-        for i in range(self.d):
-            l_ent = entropy(patch[i].astype(int), disk(self.h))
-            if m_ent < np.mean(l_ent):
-                m_ent = np.mean(l_ent)
-                ent = l_ent
+        if c==0:
+            m_ent = 0
+            ent = np.zeros([self.h,self.w])
+            for i in range(self.d):
+                l_ent = entropy(patch[i].astype(int), disk(self.h))
+                if m_ent < np.mean(l_ent):
+                    m_ent = np.mean(l_ent)
+                    ent = l_ent
+        
+            top_ent = np.percentile(ent, 90)
 
-        top_ent = np.percentile(ent, 90)
-
-        # if 80th entropy percentile = 0
-        if top_ent == 0:
-            return False  
-
+            # if 80th entropy percentile = 0
+            if top_ent == 0:
+                return False
         return True
 
     def make_patch(self, volume, center_labels):
