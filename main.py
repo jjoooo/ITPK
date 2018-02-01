9af9de2ffc82fdf4c6cbb36c8b0f18593f26978e
jui <kjhachhh@gmail.com>
2018-02-01 13:43:50 +0900

    Update main.py

--- main.py
+++ main.py
@@ -97,9 +97,9 @@ for ep in range(args.n_epoch):
     for b in val_batch:
         validation(args, b, models, ep)
 
-''' # have to edit batch func.
-# Test
+
+# Test (segmentation)
 for path in val_path:
     testing(args, path, models, idx)
-'''
+
     
